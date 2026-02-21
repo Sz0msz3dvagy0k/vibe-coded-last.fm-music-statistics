e1568b10cf2fb3d77d8a09fb7c10fbd26313c368
Junghausz Benedek <junghauszbenedek@icloud.com>
2026-02-21 21:36:24 +0100

    optimized imports

--- frontend/src/components/TopArtists.jsx
+++ frontend/src/components/TopArtists.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { lastfmAPI, spotifyAPI } from '../services/api';
 import './TopArtists.css';
 
